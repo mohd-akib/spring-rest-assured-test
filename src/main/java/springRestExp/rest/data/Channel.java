5b4af5596eaf0b4b47e162104ff9122c23ef91e4
Mohd Akib <mohd.akib@medlife.com>
2020-02-11 19:10:12 +0530

    wrote rest assured api testing for get and post

--- src/main/java/springRestExp/rest/data/Channel.java
+++ src/main/java/springRestExp/rest/data/Channel.java
@@ -1,6 +1,8 @@
 package springRestExp.rest.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,8 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Channel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/test/java/springRestExp/rest/RestApplicationTests.java
+++ src/test/java/springRestExp/rest/RestApplicationTests.java
@@ -1,13 +1,58 @@
 package springRestExp.rest;
 
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import lombok.val;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+import springRestExp.rest.data.Channel;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.hasItems;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class RestApplicationTests {
 
+	private String baseuri = "http://localhost:8080";
 	@Test
 	void contextLoads() {
 	}
+
+
+	@Test
+	public void testChannels() {
+		get(baseuri + "/channels").then()
+				.assertThat()
+				.statusCode(HttpStatus.OK.value())
+				.body("_embedded.channels.name", hasItems("sms"));
+
+
+	}
+
+	@Test
+	public void testChannelsPost() {
+		String json = "{\"name\": \"email\"}";
+		given().
+						contentType(ContentType.JSON).
+						accept(ContentType.JSON).
+						body(json).
+		when().
+						post(baseuri + "/channels").
+		then().
+						assertThat().
+						statusCode(HttpStatus.CREATED.value());
+
+
+	}
+	
+
 
 }
